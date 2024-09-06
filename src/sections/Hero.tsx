8eaf270819569b62d22202dcf74e02cc27566ea7
soumava <soumavadas06@gmail.com>
2024-09-06 20:20:23 +0530

    Hero page in progress

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,3 +1,40 @@
+import memojiImage from "@/assets/images/memoji-computer.png";
+import Image from "next/image";
+import ArrowDown from "@/assets/icons/arrow-down.svg";
+
 export const HeroSection = () => {
-  return <div>Hero Section</div>;
+  return (
+    <div className="py-32">
+      <div className="container">
+        <div className="flex flex-col items-center">
+          <Image
+            src={memojiImage}
+            className="size-[100px]"
+            alt="Person peeking from behind laptop"
+          />
+          <div className="bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex items-center gap-4 rounded-lg">
+            <div className="bg-green-500 size-2.5 rounded-full"></div>
+            <div className="text-sm font-medium">
+              Available for new projects
+            </div>
+          </div>
+        </div>
+        <h1 className="font-serif text-3xl text-center mt-8 tracking-wide">Building Exceptional User Experiences</h1>
+        <p className="mt-4 text-center text-white/60">
+          I specialize in transforming desings into funcional, high-performing
+          web applications. Let's discuss your next project.
+        </p>
+        <div className="flex flex-col items-center mt-8">
+          <button className="inline-flex items-center gap-2 border-white/15 px-6 h-12 rounded-xl">
+            <span className="font-semibold">Explore My Work</span>
+            <ArrowDown />
+          </button>
+          <button className="inline-flex items-center">
+            <span>👋</span>
+            <span>Let's Connect</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
